eca85125aae876cbacf2e8242c4b5473e5404121
ChangJoo Park <pcjpcj2@gmail.com>
2017-03-07 21:51:52 +0900

    API 요청 예제 작성

---
+++ src/examples/APIExample.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="">
+    <h1>Hello</h1>
+    {{totalProduct}}
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  created () {
+    this.fetchProduct()
+  },
+  computed: {
+    ...mapGetters([
+      'totalProduct'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'fetchProduct'
+    ])
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- src/vuex/getters.js
+++ src/vuex/getters.js
@@ -2,3 +2,4 @@ export const unreadMessagesCount = state => state.messages.main.length
 export const unreadNotificationsCount = state => state.notifications.main.length
 export const remainTasksCount = state => state.tasks.main.length
 export const currentUser = state => state.user.main
+export const totalProduct = state => state.products.main
