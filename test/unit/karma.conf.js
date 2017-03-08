55f07e305cc93fdb6045a7547db1d2867459f104
ChangJoo Park <changjoo.park@partner.so>
2017-03-08 09:42:28 +0900

    Vuex를 위한 babel-polyfill 추가

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -4,6 +4,7 @@
 //   https://github.com/webpack/karma-webpack
 
 var webpackConfig = require('../../build/webpack.test.conf')
+var babelPolyfill = require('../../node_modules/babel-polyfill/dist/polyfill.js')
 
 module.exports = function (config) {
   config.set({
@@ -14,7 +15,10 @@ module.exports = function (config) {
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    files: [
+      '../../node_modules/babel-polyfill/dist/polyfill.js',
+      './index.js'
+    ],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
