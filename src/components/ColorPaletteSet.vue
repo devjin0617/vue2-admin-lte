19daf1e41810e4f537b34e5d502cd30ad6c09bb5
devjin0617 <devjin0617@gmail.com>
2017-03-10 00:37:57 +0900

    #20 finished Color Palette Set

---
+++ src/components/ColorPaletteSet.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="col-sm-4 col-md-2">
+    <h4 class="text-center">{{ this.color }}</h4>
+
+    <div class="color-palette-set">
+      <div :class="[this.colorSet[this.color].className, 'disabled', 'color-palette']"><span>Disabled</span></div>
+      <div :class="[this.colorSet[this.color].className, 'color-palette']"><span>{{ this.colorSet[this.color].color }}</span></div>
+      <div :class="[activeMode, 'color-palette']"><span>Active</span></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorPaletteSet',
+  props: {
+    color: {
+      type: String,
+      default: 'Primary'
+    }
+  },
+  data () {
+    return {
+      colorSet: {
+        Primary: {
+          className: 'bg-light-blue',
+          color: '#3c8dbc'
+        },
+        Info: {
+          className: 'bg-aqua',
+          color: '#00a65a'
+        },
+        Success: {
+          className: 'bg-green',
+          color: '#00a65a'
+        },
+        Warning: {
+          className: 'bg-yellow',
+          color: '#f39c12'
+        },
+        Danger: {
+          className: 'bg-red',
+          color: '#f56954'
+        },
+        Gray: {
+          className: 'bg-gray',
+          color: '#d2d6de'
+        },
+        Navi: {
+          className: 'bg-navy',
+          color: '#001F3F'
+        },
+        Teal: {
+          className: 'bg-teal',
+          color: '#39CCCC'
+        },
+        Purple: {
+          className: 'bg-purple',
+          color: '#605ca8'
+        },
+        Orange: {
+          className: 'bg-orange',
+          color: '#ff851b'
+        },
+        Maroon: {
+          className: 'bg-maroon',
+          color: '#D81B60'
+        },
+        Black: {
+          className: 'bg-black',
+          color: '#111111'
+        }
+      }
+    }
+  },
+  computed: {
+    activeMode () {
+      return `${this.colorSet[this.color].className}-active`
+    }
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/ui-elements/General.vue
+++ src/pages/ui-elements/General.vue
@@ -6,129 +6,15 @@
       </div>
       <div class="box-body">
         <div class="row">
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Primary</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-light-blue disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-light-blue color-palette"><span>#3c8dbc</span></div>
-              <div class="bg-light-blue-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Info</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-aqua disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-aqua color-palette"><span>#00c0ef</span></div>
-              <div class="bg-aqua-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Success</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-green disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-green color-palette"><span>#00a65a</span></div>
-              <div class="bg-green-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Warning</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-yellow disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-yellow color-palette"><span>#f39c12</span></div>
-              <div class="bg-yellow-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Danger</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-red disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-red color-palette"><span>#f56954</span></div>
-              <div class="bg-red-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Gray</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-gray disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-gray color-palette"><span>#d2d6de</span></div>
-              <div class="bg-gray-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
+          <ColorPaletteSet v-for="color in colors[0]"
+            :color="color"
+          ></ColorPaletteSet>
         </div>
         <!-- /.row -->
         <div class="row">
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Navy</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-navy disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-navy color-palette"><span>#001F3F</span></div>
-              <div class="bg-navy-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Teal</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-teal disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-teal color-palette"><span>#39CCCC</span></div>
-              <div class="bg-teal-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Purple</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-purple disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-purple color-palette"><span>#605ca8</span></div>
-              <div class="bg-purple-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Orange</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-orange disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-orange color-palette"><span>#ff851b</span></div>
-              <div class="bg-orange-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Maroon</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-maroon disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-maroon color-palette"><span>#D81B60</span></div>
-              <div class="bg-maroon-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-4 col-md-2">
-            <h4 class="text-center">Black</h4>
-
-            <div class="color-palette-set">
-              <div class="bg-black disabled color-palette"><span>Disabled</span></div>
-              <div class="bg-black color-palette"><span>#111111</span></div>
-              <div class="bg-black-active color-palette"><span>Active</span></div>
-            </div>
-          </div>
-          <!-- /.col -->
+          <ColorPaletteSet v-for="color in colors[1]"
+            :color="color"
+          ></ColorPaletteSet>
         </div>
         <!-- /.row -->
       </div>
@@ -138,10 +24,23 @@
 </template>
 
 <script>
+import ColorPaletteSet from '../../components/ColorPaletteSet.vue'
+
 export default {
   name: 'General',
+  data () {
+    return {
+      colors: [
+        ['Primary', 'Info', 'Success', 'Warning', 'Danger', 'Gray'],
+        ['Navi', 'Teal', 'Purple', 'Orange', 'Maroon', 'Black']
+      ]
+    }
+  },
   created () {
 
+  },
+  components: {
+    ColorPaletteSet
   }
 }
 </script>
