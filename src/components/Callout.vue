4134f036313edb31c3acec2bc127a6d608a8c1f8
devjin0617 <devjin0617@gmail.com>
2017-03-10 22:23:28 +0900

    #21 Add Alerts and Callouts in General of UI Element

---
+++ src/components/Callout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="callout" :class="classObject">
+    <h4>{{ title }}</h4>
+
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+const CalloutTypes = ['success', 'info', 'warning', 'danger']
+export default {
+  name: 'Callout',
+  props: {
+    type: {
+      default: 'success',
+      validator: function (value) {
+        return CalloutTypes.includes(value)
+      }
+    },
+    title: {
+      type: String
+    },
+    text: {
+      type: String
+    }
+  },
+  computed: {
+    classObject () {
+      return {
+        'callout-success': this.type === 'success',
+        'callout-info': this.type === 'info',
+        'callout-warning': this.type === 'warning',
+        'callout-danger': this.type === 'danger'
+      }
+    }
+  }
+}
+</script>
--- src/pages/ui-elements/General.vue
+++ src/pages/ui-elements/General.vue
@@ -19,12 +19,117 @@
         <!-- /.row -->
       </div>
     </div>
+
+    <h2 class="page-header">Alerts and Callouts</h2>
+
+    <div class="row">
+      <div class="col-md-6">
+        <div class="box box-default">
+          <div class="box-header with-border">
+            <i class="fa fa-warning"></i>
+
+            <h3 class="box-title">Alerts</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+
+            <Alert
+              type="danger"
+              :dismissible="true">
+              <div slot="header">
+                <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+              </div>
+              <div slot="body">
+                Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my entire
+                soul, like these sweet mornings of spring which I enjoy with my whole heart.
+              </div>
+            </Alert>
+            <Alert
+              type="info"
+              :dismissible="true">
+              <div slot="header">
+                <h4><i class="icon fa fa-info"></i> Alert!</h4>
+              </div>
+              <div slot="body">
+                Info alert preview. This alert is dismissable.
+              </div>
+            </Alert>
+            <Alert
+              type="warning"
+              :dismissible="true">
+              <div slot="header">
+                <h4><i class="icon fa fa-warning"></i> Alert!</h4>
+              </div>
+              <div slot="body">
+                Warning alert preview. This alert is dismissable.
+              </div>
+            </Alert>
+            <Alert
+              type="success"
+              :dismissible="true">
+              <div slot="header">
+                <h4><i class="icon fa fa-check"></i> Alert!</h4>
+              </div>
+              <div slot="body">
+                Success alert preview. This alert is dismissable.
+              </div>
+            </Alert>
+
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+
+      <div class="col-md-6">
+        <div class="box box-default">
+          <div class="box-header with-border">
+            <i class="fa fa-bullhorn"></i>
+
+            <h3 class="box-title">Callouts</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+
+            <Callout
+              type="danger"
+              title="I am a danger callout!"
+              text="There is a problem that we need to fix. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart."
+            ></Callout>
+
+            <Callout
+              type="info"
+              title="I am an info callout!"
+              text="Follow the steps to continue to payment."
+            ></Callout>
+
+            <Callout
+              type="warning"
+              title="I am a warning callout!"
+              text="This is a yellow callout."
+            ></Callout>
+
+            <Callout
+              type="success"
+              title="I am a success callout!"
+              text="This is a green callout."
+            ></Callout>
+
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- /.col -->
+    </div>
   </div>
 
 </template>
 
 <script>
 import ColorPaletteSet from '../../components/ColorPaletteSet.vue'
+import Alert from '../../components/Alert.vue'
+import Callout from '../../components/Callout.vue'
 
 export default {
   name: 'General',
@@ -40,7 +145,9 @@ export default {
 
   },
   components: {
-    ColorPaletteSet
+    ColorPaletteSet,
+    Alert,
+    Callout
   }
 }
 </script>
