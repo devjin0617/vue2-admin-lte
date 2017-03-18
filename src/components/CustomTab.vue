104cfb1b507878e35515bf2bdadda506a714b82d
devjin0617 <devjin0617@gmail.com>
2017-03-18 20:00:13 +0900

    Add Custom Tabs

---
+++ src/components/CustomTab.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="nav-tabs-custom">
+    <ul class="nav nav-tabs">
+      <li v-for="(tab, index) in tabList" :class="tab.class">
+        <a :href="tab.id" data-toggle="tab">{{tab.title}}</a>
+      </li>
+      <slot name="nav"></slot>
+    </ul>
+    <div class="tab-content">
+      <slot name="content" class="tab-pane"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomTab',
+  props: {
+
+  },
+  data () {
+    return {
+      tabList: []
+    }
+  },
+  mounted () {
+    for (const i in this.$slots.content) {
+      var vm = this.$slots.content[i]
+
+      if (typeof vm !== 'object') {
+        continue
+      }
+      vm.elm.className += ' tab-pane'
+      this.tabList.push({
+        id: '#' + vm.data.attrs.id,
+        class: vm.data.staticClass || '',
+        title: vm.data.attrs.title
+      })
+    }
+  }
+}
+</script>
+
