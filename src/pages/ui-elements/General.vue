a524c4a77dc80c85d207652b3cf113c26893fb2a
devjin0617 <devjin0617@gmail.com>
2017-03-29 00:20:05 +0900

    Add Description and Description Horizontal in Typography

--- src/pages/ui-elements/General.vue
+++ src/pages/ui-elements/General.vue
@@ -741,6 +741,65 @@
       </div>
     </div>
 
+    <div class="row">
+
+      <div class="col-md-6">
+
+        <Box
+          title="Description"
+          :isBorder="true"
+          widgetType="">
+          <i slot="icon-title" class="fa fa-text-width"></i>
+
+          <div slot="content">
+
+            <dl>
+              <dt>Description lists</dt>
+              <dd>A description list is perfect for defining terms.</dd>
+              <dt>Euismod</dt>
+              <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+              <dd>Donec id elit non mi porta gravida at eget metus.</dd>
+              <dt>Malesuada porta</dt>
+              <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
+            </dl>
+
+          </div>
+
+        </Box>
+      </div>
+
+
+      <div class="col-md-6">
+
+        <Box
+          title="Description Horizontal"
+          :isBorder="true"
+          widgetType="">
+          <i slot="icon-title" class="fa fa-text-width"></i>
+
+          <div slot="content">
+
+            <dl class="dl-horizontal">
+              <dt>Description lists</dt>
+              <dd>A description list is perfect for defining terms.</dd>
+              <dt>Euismod</dt>
+              <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+              <dd>Donec id elit non mi porta gravida at eget metus.</dd>
+              <dt>Malesuada porta</dt>
+              <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
+              <dt>Felis euismod semper eget lacinia</dt>
+              <dd>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
+                sit amet risus.
+              </dd>
+            </dl>
+
+          </div>
+
+        </Box>
+      </div>
+
+    </div>
+
   </div>
 
 </template>
