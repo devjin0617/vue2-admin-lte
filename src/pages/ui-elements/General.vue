30294f71a3fb760ca1d0ed131cf9ebf5843dda9c
devjin0617 <devjin0617@gmail.com>
2017-03-29 00:14:09 +0900

    Add Block Quotes and Block Quotes Pulled Right in Typgraphy

--- src/pages/ui-elements/General.vue
+++ src/pages/ui-elements/General.vue
@@ -592,8 +592,53 @@
           </div>
         </Box>
 
+
+
       </div>
 
+    </div>
+
+    <div class="row">
+
+      <div class="col-md-6">
+
+        <Box
+          title="Block Quotes"
+          :isBorder="true"
+          widgetType="">
+          <i slot="icon-title" class="fa fa-text-width"></i>
+
+          <div slot="content">
+
+              <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+              </blockquote>
+
+          </div>
+        </Box>
+
+      </div>
+
+      <div class="col-md-6">
+
+        <Box
+          title="Block Quotes Pulled Right"
+          :isBorder="true"
+          widgetType="">
+          <i slot="icon-title" class="fa fa-text-width"></i>
+
+          <div slot="content">
+
+              <blockquote class="pull-right">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+              </blockquote>
+
+          </div>
+        </Box>
+
+      </div>
 
     </div>
 
