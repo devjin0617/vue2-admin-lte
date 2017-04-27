77f9a04118485322dc8bace7af66171176eb6aa8
devjin0617 <devjin0617@gmail.com>
2017-04-28 08:46:25 +0900

    Add slide item

---
+++ src/components/VASlideItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="treeview">
+    <a href="#">
+      <i class="fa fa-laptop"></i>
+      <span>UI Elements</span>
+      <span class="pull-right-container">
+        <i class="fa fa-angle-left pull-right"></i>
+      </span>
+    </a>
+    <ul class="treeview-menu">
+      <router-link tag="li" :to="{ name: 'General' }">
+        <a href=""><i class="fa fa-circle-o"></i> General</a>
+      </router-link>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'va-slide-item,
+  created () {
+
+  }
+}
+</script>
