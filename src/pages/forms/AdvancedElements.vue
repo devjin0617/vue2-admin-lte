47a61f623c8d9a12c2b65f4cefbb47f19d02045e
devjin0617 <devjin0617@gmail.com>
2017-05-11 21:28:55 +0900

    #32: Create Advanced Elements View and Update router

---
+++ src/pages/forms/AdvancedElements.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    AdvancedElements View
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'advanced-elements',
+  created () {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
